5c25fc1e092ce0a79fb6b6f26ec801978fad19b2
PovilasC <polass47@gmail.com>
2021-09-10 20:41:35 +0300

    lwv

--- rest/src/main/java/com/example/rest/product/Product.java
+++ rest/src/main/java/com/example/rest/product/Product.java
@@ -11,21 +11,34 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
      method = POST,
      headers = "Accept=application/json")
 
-@JsonComponent
+//@JsonComponent
 public class Product {
     CustomerDetails customerDetails;
     @PostMapping("get/products")
-    public CustomerDetails postBody(@RequestBody CustomerDetails customerDetails) {
+    public String postBody(@RequestBody CustomerDetails customerDetails) {
 //        System.out.print(customerDetails);
         int age = customerDetails.age;
         int income = customerDetails.income;
         boolean student = customerDetails.student;
-        if(student == true && income > 100) {
-            System.out.print("yes");
-        } else {
-            System.out.print("no");
+
+        String product = "";
+        // Junior Saver Account
+        if(age < 18){
+            product = "Junior Saver Account";
         }
-        return customerDetails;
+        // Current Account Plus
+       if(income > 40000 && age > 17){
+           product = "Current Account Plus";
+       }
+        // Student Account
+        if(student == true && age > 17){
+            product = "Student Account";
+        }
+        // Senior Account
+        if(age >= 65){
+            product = "Senior Account";
+        }
+        return product;
     }
 
 

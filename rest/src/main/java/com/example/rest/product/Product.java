9a835042b78568786b88821af6ce4147e8edf210
PovilasC <polass47@gmail.com>
2021-09-13 12:58:39 +0300

    lwv

--- rest/src/main/java/com/example/rest/product/Product.java
+++ rest/src/main/java/com/example/rest/product/Product.java
@@ -12,38 +12,28 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
      method = POST,
      headers = "Accept=application/json")
 
-//@JsonComponent
+@JsonComponent
 public class Product {
     CustomerDetails customerDetails;
     @PostMapping("get/products")
     public String makeOffer(@RequestBody CustomerDetails customerDetails) throws Exception{
         ObjectMapper objectMapper = new ObjectMapper();
-//        System.out.print(customerDetails);
+        System.out.print(customerDetails);
         int age = customerDetails.age;
         int income = customerDetails.income;
         boolean student = customerDetails.student;
-        System.out.print(customerDetails.student);
         String product = "";
 
-        // Senior Account
-        if(age >= 65 && student == false||true){
-            product = "Senior Account";
+        if(age < 18) {
+            product = "Young";
         }
 
-        // Junior Saver Account
-        if(age < 18){
-            product = "Junior Saver Account";
+        if(age > 65) {
+            product = "Old";
         }
 
-        // Current Account Plus
-       if(income > 40000 && age > 17 && student == false||true){
-           product = "Current Account Plus";
-       }
-        // Student Account
-        if(student == true && age > 17){
-            product = "Student Account";
-        }
-        return objectMapper.writeValueAsString(product);
+        System.out.println(product);
+        return product;
     }
 
 }
--- rest/src/test/java/com/example/rest/product/ProductTest.java
+++ rest/src/test/java/com/example/rest/product/ProductTest.java
@@ -1,13 +1,14 @@
 package com.example.rest.product;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.example.rest.product.CustomerDetails;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.core.JsonParseException;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -17,6 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
@@ -24,40 +26,62 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+//import com.google.code.gson;
+
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(Product.class)
 class ProductTest {
-    CustomerDetails customerDetails;
-    @Test
-    // testing if we get the right offer based on the input
-    void checkConditionMatch() throws Exception{
+//    @Test
+//    // testing if we get the right offer based on the input
+//    void checkConditionMatch() throws Exception{
+//
+//    CustomerDetails customerDetails = new CustomerDetails(77, false, 200);
+//        Product product = new Product();
+//        String response = product.makeOffer(customerDetails);
+//        assertEquals("Senior Account", response);
+//    }
 
-     customerDetails = new CustomerDetails(77, false, 200);
-        Product product = new Product();
-        String response = product.makeOffer(customerDetails);
-        assertEquals("Senior Account", response);
-    }
     @Autowired
     private MockMvc mvc;
     private MvcResult mvcResult;
+    private CustomerDetails customerDetails;
+    Gson gson = new Gson();
 
     @Test
     void hello() throws Exception {
-        CustomerDetails customerDetails = new CustomerDetails(77, false, 200);
-        RequestBuilder request = post("/api/get/products" ).requestAttr("person", customerDetails);
-            MvcResult result = mvc.perform(request).andReturn();
-            assertEquals("Senior Account", result.getResponse().getContentAsString());
+        // object to test with
+        customerDetails = new CustomerDetails(17, false, 0);
+        mvcResult = mvc.perform(post("/api/get/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(gson.toJson(customerDetails)))
+                .andExpect(status().isOk())
+//                .andExpect(content().string(gson.toJson("Old"))).andReturn();
+                .andExpect(content().string("Young")).andReturn();
+    }
+
+    @Test
+    void hello2() throws Exception {
+        // object to test with
+        customerDetails = new CustomerDetails(77, false, 0);
+        mvcResult = mvc.perform(post("/api/get/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(customerDetails)))
+                .andExpect(status().isOk())
+//                .andExpect(content().string(gson.toJson("Old"))).andReturn();
+                .andExpect(content().string("Old")).andReturn();
+
     }
 
     @Test
     void offerForJunior() throws Exception {
-        CustomerDetails customerJunior = new CustomerDetails(16, false, 0);
+
+        customerDetails = new CustomerDetails(16, false, 0);
         mvcResult = mvc.perform(post("/api/get/products")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(mapToJson(customerJunior )))
+                .content(mapToJson(customerDetails )))
                 .andExpect(status().isOk()).andReturn();
-        assertEquals("Junior Saver Account", mvcResult.getResponse().getContentAsString());
+//        assertEquals("Junior Saver Account", mvcResult.getResponse().getContentAsString());
     }
 
     @Test
@@ -65,16 +89,23 @@ class ProductTest {
         CustomerDetails customerSenior = new CustomerDetails(65, false, 0);
         mvcResult = mvc.perform(post("/api/get/products")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(mapToJson(customerSenior )))
-                .andExpect(status().isOk()).andExpect(content().string(mapToJson("Current Account Plus"))).andReturn();
-//        String content = mvcResult.getResponse().getContentAsString();
-//        assertEquals("Senior Account", content);
+                        .content(gson.toJson(customerSenior)))
+                .andExpect(status().isOk()).andExpect(content().string(mapToJson("Senior Account"))).andReturn();
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals("Senior Account", content);
+
 
     }
 
     protected String mapToJson(Object obj) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(obj);
+    }
+    protected <T> T mapFromJson(String json, Class<T> clazz)
+            throws JsonParseException, JsonMappingException, IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, clazz);
     }
 
 }
